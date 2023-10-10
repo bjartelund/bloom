5a0482b6d14a3b2759e53b077bfabcf8887a2db7
Bjarte Aarmo Lund <bjarte@godkjemi.net>
2023-10-10 20:47:14 +0200

    Working example

--- Lib/BloomLib.cs
+++ Lib/BloomLib.cs
@@ -1,5 +1,52 @@
-﻿namespace bloom;
-public class Class1
-{
+﻿using System.Collections;
 
+namespace bloom;
+public class Bloom
+{
+    public Bloom(ushort hashes, ushort numberOfBits)
+    {
+        Hashes = hashes;
+        NumberOfBits = numberOfBits;
+
+        bitVector = new BitArray(numberOfBits);
+    }
+
+    public ushort Hashes { get; }
+    public ushort NumberOfBits { get; }
+    public ushort NumberOfHashesAdded = 0;
+
+    public BitArray bitVector;
+
+    public void Insert(object element) {
+        var indices = FindIndices(element);
+        PopulateBitVector(indices);
+        NumberOfHashesAdded += 1;
+    }
+
+    public bool Search(object element) {
+        var indices = FindIndices(element);
+        return indices.All(i=>bitVector[i]);
+    }
+    public double FalsePositiveLikelihood() => (double) Math.Pow(1.0d -Math.Pow(Math.E,(double)Hashes* NumberOfHashesAdded / NumberOfBits), Hashes);
+
+
+    private IEnumerable<ushort> FindIndices(object element) {
+        for (int i = 0; i < Hashes; i++)
+        {
+            var hash = (i,element).GetHashCode();
+            var remainder = Modulo(hash);
+            yield return remainder;
+        }
+    }
+    private ushort Modulo (int a) {
+        var b = (int)NumberOfBits;
+        var mod = a - b*Math.Floor(a / (double)b);
+        return (ushort) mod;
+    }
+    private void PopulateBitVector(IEnumerable<ushort> indices) {
+        foreach (var index in indices)
+        {
+            bitVector[index] = true;
+        }
+    }
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,26 @@
+using bloom;
+
 namespace Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Existing_Elements_Should_Be_Found()
     {
+        var bloom = new Bloom(2,14);
+        bloom.Insert("abc");
+        bloom.Insert("def");
+        var search = bloom.Search("def");
+        search.Should().BeTrue();
+    }
 
+    [Fact]
+    public void Non_Existing_Elements_Should_Return_False()
+    {
+        var bloom = new Bloom(2,14);
+        bloom.Insert("abc");
+        bloom.Insert("def");
+        var search = bloom.Search("gfh");
+        search.Should().BeFalse();
     }
 }
